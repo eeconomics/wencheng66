56aebca20f9b95db0d0b4bdfb89a5b689ecce977
fengwencheng66 <116816727+fengwencheng66@users.noreply.github.com>
2025-10-02 21:20:00 +0800

    修订config.py

--- source/conf.py
+++ source/conf.py
@@ -5,7 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 import recommonmark
@@ -17,7 +19,7 @@ author = '文成'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark']
+#extensions = ['recommonmark']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -27,7 +29,7 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 # html_theme = "press"
 # html_static_path = ['_static']
 
